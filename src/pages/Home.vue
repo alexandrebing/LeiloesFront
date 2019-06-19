354591270fccd8f6ff1c4cfb5259cb80b7936a0a
Alexandre Scheer Bing <alexandrebing@gmail.com>
2019-06-19 20:28:17 -0300

    basic auction system

---
+++ src/pages/Home.vue
@@ -0,0 +1,39 @@
+<template>
+<div>
+
+    <h1>Leiloes abertos:</h1>
+    <div :key="item.name" v-for="item in items">
+        <li> {{item.name}} - {{item.currentBid}}</li>
+        <input type="number" name="" id="" placeholder="make your bid">
+        <button>make a bid</button>
+    </div>
+    
+</div>
+    
+</template>
+
+
+<script>
+export default {
+    data(){
+        return{
+            items: [{
+                name: "Piano",
+                currentBid: 3200
+             },{
+                 name: "Porshe Carrera",
+                 currentBid: 77000
+             } , {
+                 name: "Quadro Monert",
+                 currentBid: 620000
+                 }]
+        }
+    }
+    
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ src/pages/Login.vue
@@ -0,0 +1,28 @@
+<template>
+
+<div>
+    <h1>Bem vindo ao gerenciador de leiloes
+
+    </h1>
+    <input type="text" name="" id="" placeholder="Usuário">
+    <input type="text" placeholder="senha">
+    <button @click="validate">Entrar</button>
+</div>
+    
+</template>
+
+<script>
+export default {
+
+    methods:{
+        validate(){
+            this.$router.push('\home')
+        }
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
